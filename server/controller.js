8e5a25fb192c295c7275647739dba1e013137b30
Jeff Jardine <47231425+3Jardine@users.noreply.github.com>
2019-04-16 00:55:26 -0700

    Put together microservices
    still having issues with navbar and reservation webpacks

--- server/controller.js
+++ server/controller.js
@@ -1,6 +1,44 @@
 const axios = require('axios');
 
 const controller = {
+  getNavbar: (req, res) => {
+    axios
+      .get(`http://localhost:3001/api/navbar`)
+      .then(response => res.status(200).send(response.data))
+      .catch(err => res.status(404).send(err));
+  },
+
+  getNavbarAndId: (req, res) => {
+    let { id } = req.params;
+    axios
+      .get(`http://localhost:3001/api/navbar/${id}`)
+      .then(response => res.status(200).send(response.data))
+      .catch(err => res.status(404).send(err));
+  },
+
+  getReservation: (req, res) => {
+    axios
+      .get(`http://localhost:3002/api/reservation`)
+      .then(response => res.status(200).send(response.data))
+      .catch(err => res.status(404).send(err));
+  },
+
+  getReservationAndId: (req, res) => {
+    let { id } = req.params;
+    axios
+      .get(`http://localhost:3002/api/reservation/id/${id}`)
+      .then(response => res.status(200).send(response.data))
+      .catch(err => res.status(404).send(err));
+  },
+
+  getDescription: (req, res) => {
+    let { id } = req.params;
+    axios
+      .get(`http://localhost:3003/api/restaurant/${id}`)
+      .then(response => res.status(200).send(response.data))
+      .catch(err => res.status(404).send(err));
+  },
+
   getReviews: (req, res) => {
     let { id } = req.params;
     axios
--- server/router.js
+++ server/router.js
@@ -1,6 +1,14 @@
 const router = require('express').Router();
 const controller = require('./controller.js');
 
-router.route('/restaurant/:id').get(controller.getReviews);
+router.route('/navbar').get(controller.getNavbar);
+router.route('/navbar/:id').get(controller.getNavbarAndId);
+
+router.route('/reservation').get(controller.getReservation);
+router.route('/reservation/:id').get(controller.getReservationAndId);
+
+router.route('/description').get(controller.getDescription);
+
+router.route('/reviews/:id').get(controller.getReviews);
 
 module.exports = router;
